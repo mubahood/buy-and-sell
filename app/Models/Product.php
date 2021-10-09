81abb0e947de4594c26137b047e9e0f7b76d4b75
Muhindo Mubaraka <mubs0x@gmail.com>
2021-10-09 16:32:40 +0300

    new changes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -51,7 +51,7 @@ class Product extends Model
             if (strlen($this->thumbnail) > 3) {
                 $thumb = json_decode($this->thumbnail);
                 if (isset($thumb->src)) {
-                    $thumbnail = URL::asset('storage/'.str_replace("public/", "", $thumb->src));
+                    $thumbnail = URL::asset('storage/thumb_'.str_replace("public/", "", $thumb->src));
                     
                 }
             }
